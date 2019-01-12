3716545539e06d91434de8704ee53031e2caefda
Dom Serra <sig647@gmail.com>
2019-01-12 15:00:16 -0500

    Still learning. Relationships now.

--- server.js
+++ server.js
@@ -7,14 +7,24 @@ app.use(cors());
 
 const schema = gql`
   type Query {
+    users: [User!]
     me: User
     user(id: ID!): User
+
+    messages: [Message!]!
+    message(id: ID!): Message!
   }
 
   type User {
     id: ID!
     username: String!
     greet: String!
+  }
+
+  type Message {
+    id: ID!
+    text: String!
+    userId: ID!
   }
 `;
 
@@ -28,21 +38,48 @@ let users ={
     id: '2',
     username: 'Bolas',
     greet: 'Its Bolas tha second',
+  },
+  3: {
+    id: '3',
+    username: 'fid',
+    greet: 'a cusser up every crack',
+  }
+}
+
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hullooo Worlddd',
+  },
+  2: {
+    id: '2',
+    text: 'second msgg',
+  },
+  3: {
+    id: '3',
+    text: 'Thirdy message ',
   }
 }
 
 const resolvers = {
   Query: {
-    me: () => me,
-    user: (parent, args) => {
-      return {username: 'Bolas'}
-    }
+    users: () => Object.values(users),
+    me: (p, a, {me}) => me,
+    user: (parent, {id}) => users[id],
+    messages: () => Object.values(messages),
+    message: (p, {id}) => messages[id],
   },
+  Message: {
+    userId: (p, a, {id}) => id
+  }
 };
 
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
+  context: {
+    me: users[1],
+  },
 });
 
 server.applyMiddleware({app, path: '/graphql'});
